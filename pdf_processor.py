30d68ca16a8e4e5b51bc11f1a0a88ec3f82ce990
binarymath <arthguins@gmail.com>
2023-05-06 20:24:53 -0300

    feat: Insert plano_de_aula pdf file and created directory pdf

--- pdf_processor.py
+++ pdf_processor.py
@@ -1,13 +1,58 @@
+from dotenv import load_dotenv
+import os
 from fpdf import FPDF
-from PyPDF2 import PdfReader
+import PyPDF2
+import requests
 
-reader = PdfReader("request.pdf")
-page = reader.pages[0]
-text = page.extract_text()
+load_dotenv()
+API_Key = os.getenv('API-Key')
+API_Host = os.getenv('API-Host')
+
+# division_of_line = input("Type the keyword to start the question: ")
+
+url = "https://openai80.p.rapidapi.com/chat/completions"
+
+with open('pdf/planodeaula.pdf', 'rb') as file:
+    pdf_reader = PyPDF2.PdfReader(file)
+
+    text = []
+    for page in range(len(pdf_reader.pages)):
+        text_list = pdf_reader.pages[page].extract_text().split('\n')
+        new_text_list = []
+        for i, line in enumerate(text_list):
+            # if division_of_line in line:
+            #     new_text_list.append('\n' + line + '\n')
+            # else:
+            new_text_list.append(line)
+        text.extend(new_text_list)
+    text = ' '.join(text)
+
+payload = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "user",
+            "content": text
+        }
+    ]
+}
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": API_Key,
+    "X-RapidAPI-Host": API_Host
+}
+response = requests.post(url, json=payload, headers=headers, timeout=1000)
+data = response.json()
+
+text = data['choices'][0]['message']['content']
+
+print("Arquivo gerado com sucesso")
 
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, text)
-pdf.output("output.pdf")
-
+max_width = 180
+lines = pdf.multi_cell(max_width, 5, text)
+for line in lines:
+    pdf.cell(0, 5, line)
+pdf.output("pdf/plano_de_aula_feito.pdf")
