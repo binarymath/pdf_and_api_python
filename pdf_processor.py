88e1e8abd33958c27c87895fe1bf50a5f5a82620
binarymath <arthguins@gmail.com>
2023-05-07 09:44:31 -0300

    fix: Used pylint

--- pdf_processor.py
+++ pdf_processor.py
@@ -1,33 +1,39 @@
-from dotenv import load_dotenv
+"""
+PDF processor, reads the pdf
+Makes the request to the api and returns another pdf with the response.
+"""
+
 import os
+from dotenv import load_dotenv
 from fpdf import FPDF
-import PyPDF2
 import requests
+import PyPDF2
+
 
 load_dotenv()
 API_Key = os.getenv('API-Key')
 API_Host = os.getenv('API-Host')
 
-url = "https://openai80.p.rapidapi.com/chat/completions"
+URL = "https://openai80.p.rapidapi.com/chat/completions"
 
 with open('pdf/plano_de_aula.pdf', 'rb') as file:
     pdf_reader = PyPDF2.PdfReader(file)
 
-    text = []
-    for page in range(len(pdf_reader.pages)):
+    TEXT = []
+    for page, _ in enumerate(pdf_reader.pages):
         text_list = pdf_reader.pages[page].extract_text().split('\n')
         new_text_list = []
         for i, line in enumerate(text_list):
             new_text_list.append(line)
-        text.extend(new_text_list)
-    text = ' '.join(text)
+        TEXT.extend(new_text_list)
+    TEXT = ' '.join(TEXT)
 
 payload = {
     "model": "gpt-3.5-turbo",
     "messages": [
         {
             "role": "user",
-            "content": text
+            "content": TEXT
         }
     ]
 }
@@ -36,18 +42,18 @@ headers = {
     "X-RapidAPI-Key": API_Key,
     "X-RapidAPI-Host": API_Host
 }
-response = requests.post(url, json=payload, headers=headers, timeout=1000)
+response = requests.post(URL, json=payload, headers=headers, timeout=1000)
 data = response.json()
 
-text = data['choices'][0]['message']['content']
+TEXT = data['choices'][0]['message']['content']
 
-print("Arquivo gerado com sucesso")
+print("File generated successfully")
 
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial", size=12)
-max_width = 180
-lines = pdf.multi_cell(max_width, 5, text)
+MAX_WIDTH = 180
+lines = pdf.multi_cell(MAX_WIDTH, 5, TEXT)
 for line in lines:
     pdf.cell(0, 5, line)
 pdf.output("pdf/plano_de_aula_feito.pdf")
